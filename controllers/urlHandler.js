92e07bb9bd018e697b88eeb6f35602270b469e17
HermanFassett <hermanfassett@gmail.com>
2015-12-16 20:59:44 -0800

    Add logs for testing

--- controllers/urlHandler.js
+++ controllers/urlHandler.js
@@ -5,6 +5,8 @@ function UrlHandler () {
 	this.getUrl = function(req, res) {
     // Set up parameters
     var input = req.params.url;
+		console.log(req.params);
+		console.log(rew.query);
     if (req.params["0"]) input += req.params["0"];
     var head = req.params.head || "http:";
     var full_url = head + "//" + input;
